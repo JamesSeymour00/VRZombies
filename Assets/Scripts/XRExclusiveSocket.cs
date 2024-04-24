c331c1534c8beb504a8b00dea2d71791c90f66f8
JamesSeymour00 <114984746+JamesSeymour00@users.noreply.github.com>
2024-04-24 01:14:20 +0100

    Sockets and Interactions

---
+++ Assets/Scripts/XRExclusiveSocket.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRExclusiveSocket : XRSocketInteractor
+{
+	[SerializeField]
+	string _acceptedTag;
+
+	// Generate the visual mesh if true
+	public override bool CanHover(IXRHoverInteractable interactable)
+	{
+		if (!base.CanHover(interactable)) return false;
+
+		if (interactable.transform.tag == _acceptedTag) return true;
+
+		return false;
+	}
+
+	// Attaches obejct to socket if true
+	public override bool CanSelect(IXRSelectInteractable interactable)
+	{
+		if (!base.CanSelect(interactable)) return false;
+
+		if (interactable.transform.tag == _acceptedTag) return true;
+
+		return false;
+	}
+}
